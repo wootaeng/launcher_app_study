3e2550b461a713ab8deb4d9abeeb7f5657eef010
wootaeng <karufuci@gmail.com>
2021-07-07 17:31:07 +0900

    adapter 추가중

---
+++ app/src/main/java/com/peng/plant/wattstore/view/BindingViewHolder.java
@@ -0,0 +1,6 @@
+package com.peng.plant.wattstore.view;
+
+import androidx.databinding.ViewDataBinding;
+
+public class BindingViewHolder extends ViewDataBinding {
+}
---
+++ app/src/main/java/com/peng/plant/wattstore/view/ListViewAdapter.java
@@ -0,0 +1,4 @@
+package com.peng.plant.wattstore.view;
+
+public class ListViewAdapter {
+}
